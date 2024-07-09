e3ebe6aff31638357dd652d1159723815bac00ef
JanisJRhea <janisrhea26@gmail.com>
2024-07-09 09:04:56 +0530

    Initial commit

---
+++ jsx/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import Component from './Component';
+
+function App() {
+  return (
+    <div className="App">
+      <Component />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ jsx/src/Component.js
@@ -0,0 +1,13 @@
+// src/Component.js
+import React from 'react';
+
+function Component() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is a simple React component using JSX.</p>
+    </div>
+  );
+}
+
+export default Component;
